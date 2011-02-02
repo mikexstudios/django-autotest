417b567da5791b5856d2aae18ca4dc37a24650f3
Michael Huynh <mike.huynh@gmail.com>
2011-02-02 11:21:46 -0500

    Forgot to add trailing comma.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name = 'django_autotest',
     packages = find_packages(),
     version = '1.0.0',
-    description = 'Adds a command to manage.py to automatically run tests whenever project file changes are detected.'
+    description = 'Adds a command to manage.py to automatically run tests whenever project file changes are detected.',
     author = 'Michael Huynh',
     author_email = 'mike@mikexstudios.com',
     url = 'http://github.com/mikexstudios/django-autotest',
